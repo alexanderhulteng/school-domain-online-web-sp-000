95249962008adfe0f80c9847dda838204f366489
Alexander Hulteng <alexanderhulteng@gmail.com>
2021-05-21 20:49:43 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,26 @@
-# code here!
+require 'pry'
+
+class School
+  attr_accessor :name, :grade
+  roster = {
+
+  }
+
+  def initialize(name)
+    @name = name
+
+  end
+
+
+
+  def add_student(name, grade)
+
+    studentRoster[grade] = []
+    studentRoster[grade] << name
+
+  end
+binding.pry
+
+
+end
+  
