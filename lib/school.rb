e4e22543bb103e14342a95d2e6e06cd5c8ffb584
Alexander Hulteng <alexanderhulteng@gmail.com>
2021-05-21 21:48:38 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,20 +1,22 @@
 require 'pry'
 
 class School
-  #attr_accessor :name, :grade
-  roster = {
-
-  }
+  attr_accessor :name, :grade
+  
 
   def initialize(name)
     @name = name
 
   end
 
+  def roster
+    @studentroster = {
 
+    }
+  end
 
   def add_student(name, grade)
-
+    
     roster[grade] = []
     roster[grade] << name
 
