7d2a6cab9c8bb6116223427c411eea59df006b2c
alexanderhulteng <alexanderhulteng@gmail.com>
2021-07-06 12:56:07 -0600

    commit tree b5f22f269c62b0d8bd296e30b40481d21b4b9e6f

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,27 @@
-# code here!
+require 'pry'
+
+class School
+  attr_accessor :name, :grade
+
+
+  def initialize(name)
+    @name = name
+
+  end
+
+  def roster
+    @studentroster = {
+
+    }
+  end
+
+  def add_student(name, grade)
+
+    roster[grade] = []
+    roster[grade] << name
+
+  end
+
+
+
+end
